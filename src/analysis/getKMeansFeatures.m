40a8011ea107f4e8a2fef020926ebdf93d07ddff
haoyio <haoyi@stanford.edu>
2014-12-03 04:05:52 -0800

    combined gda and svm fncs to single fnc

--- src/analysis/getKMeansFeatures.m
+++ src/analysis/getKMeansFeatures.m
@@ -1,22 +1,21 @@
-function [X, Y] = svmDPMeans(x, y)
+function [X, Y] = getKMeansFeatures(y)
 % Use
 %   Returns the k-means compressed data-trained SVM model.
 % Input
-%   x : m-samples training set, where each row is a sample feature vector
 %   y : m-samples training set vector, where each element is a sample label
 % Output
 %   X : feature vector for each game
 %   Y : classification label for each game
 
     % constants
-    % LAMBDA = (1 + sqrt(5)) / 2; % ~23 clusters
-    % LAMBDA = 2; % ~15 clusters
-    LAMBDA = 2.5; % ~ 8 clusters
+    KMEANS_DATAFILE = 'results-kfold-training-full';
+    KMEANS_INDEX = 2;
     
-    % get cluster labels
-    [c, ~, ~] = myDPMeans(x, LAMBDA);
+    % load k-means results
+    kmeansModel = load(KMEANS_DATAFILE);
+    c = kmeansModel.results{KMEANS_INDEX}.c;
     
     % get team composition-based features and labels
     [X, Y] = clusterLabelsToFeatures(c, y);
     
-end % function svmDPMeans
+end % function getKMeansFeatures
---
+++ src/analysis/runTeamCompPred.m
@@ -0,0 +1,68 @@
+function [pred, predObj] = runTeamCompPred(compressor, classifier)
+% Use
+%   Runs classifier with features selected with k-means, DP-means, or EM
+%   with Gaussian mixture model data compression methods.
+% Input
+%   compressor : string indicating k-means ('kmeans'), DP-means 
+%                ('dpmeans'), EM Gaussian mixture model ('emgmm')
+% Output
+%   pred : predictor function handle that outputs a win/loss (1/0)
+%          classification based on the input data point
+%   predObj : classification model object
+
+    % constants
+    DATAFILE = '../lolapi/training_full.csv';
+    KMEANS = 'kmeans';
+    DPMEANS = 'dpmeans';
+    EMGMM = 'emgmm';
+    SVM = 'svm';
+    GDA = 'gda';
+    LR = 'logisticRegression';
+    
+    % check input arguments
+    if ~strcmp(compressor, KMEANS) && ...
+       ~strcmp(compressor, DPMEANS) && ...
+       ~strcmp(compressor, EMGMM)
+        error(['Error: compressor must be a valid string indicating ' ...
+               'the data compression method']);
+    elseif ~strcmp(classifier, SVM) && ...
+           ~strcmp(classifier, GDA) && ...
+           ~strcmp(classifier, LR)
+        error(['Error: classifier must be a valid string indicating ' ...
+               'the classification method']);
+    end % if
+    
+    % load training data
+    dataset = csvread(DATAFILE, 1, 0);
+    x = dataset(:, 2:end);
+    y = dataset(:, 1);
+    
+    % get compressed features and labels
+    startTime = tic;
+    if strcmp(compressor, KMEANS)
+        [X, Y] = getKMeansFeatures(y);
+    elseif strcmp(compressor, DPMEANS)
+        [X, Y] = getDPMeansFeatures(x, y);
+    elseif strcmp(compressor, EMGMM)
+        % TODO: code EM algorithm up
+        error('Error: EM algorithm not implemented!');
+    else
+        assert(false, 'Assertion failed: Shouldn''t reach here');
+    end % if
+    
+    %  train classifier model and convert predictor to function handle
+    if strcmp(classifier, SVM)
+        predObj = fitcsvm(X, Y);
+    elseif strcmp(classifier, GDA)
+        predObj = fitcdiscr(X, Y);
+    elseif strcmp(classifier, LR)
+        % TODO: code logistic regression algorithm up
+        error('Error: logistic regression algorithm not implemented!');
+    end % if
+    
+    pred = @(z)predict(predObj, z);
+    
+    fprintf(['\n%s model trained with %s data compression method in ' ...
+             '%.2f sec\n'], classifier, compressor, toc(startTime));
+
+end % function runGDA
